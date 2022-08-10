9c51574f4bf7cb92b2b510d3e2265d031fa897d0
Aleksandra Vorontsova <alexandrettio@gmail.com>
2022-08-11 00:24:35 +0300

    Skip test

--- tests/chat_test.py
+++ tests/chat_test.py
@@ -1,3 +1,6 @@
+import pytest
+
+
 def test_create_message(sleep_a_bit, group_create, group_leave):
     user, _ = group_create
     response = user.group.chat.create("party", "TestMessage")
@@ -28,8 +31,9 @@ def test_like(sleep_a_bit, group_join, message_create, group_leave):
     assert len(response.data.likes) == 1
 
 
+@pytest.mark.skip(reason="After couple running of this test the user2 will be banned :D ")
 def test_mark_spam(sleep_a_bit, group_join, message_create, group_leave):
     user1, user2, message_id = message_create
-    response = user2.group.chat.mark_as_spam("party", message_id)
+    response = user2.group.chat.mark_as_spam("party", message_id, comment="I'm testing API")
     assert response.success
     assert len(response.data.flags) == 1
